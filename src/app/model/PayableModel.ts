aaf0c648f3207d4f6368e420b00dab09b5b2cf18
Cleibson Gomes <cleibson.gomes.silva@gmail.com>
2019-09-16 14:04:10 -0300

    Fluxo de Transação completo
    Ainda trabalhando no processo da transação.

--- src/app/model/PayableModel.ts
+++ src/app/model/PayableModel.ts
@@ -1,38 +1,42 @@
-import {Column, DataType, Model, Table} from "sequelize-typescript";
+import {Column, DataType, ForeignKey, Model, Table} from "sequelize-typescript";
+import {Transaction} from "./TransactionModel";
 
 @Table({tableName: "payable"})
 export class Payable extends Model<Payable> {
-    static sequelize: any
+    static sequelize: any;
 
     @Column({
         type : DataType.ENUM,
         values: ['1', '2', '3'],
         allowNull: false,
         defaultValue: 3,
-        field: "transaction_type",
         comment : '1 -> paid , 2 -> waiting_funds, 3 -> unknown'
     })
     status: number;
 
     @Column( {
         allowNull: false,
-        field: "dt_payment",
         comment : 'Data do pagamento'
     } )
     dtPayment: Date;
 
     @Column({
         allowNull: false,
-        field: "percent_rate",
         comment: "Porcetagem de taxa"
     })
     percentRate: number;
 
     @Column({
         allowNull: false,
-        field: "vlr_payable",
         comment: "Valor da Transação final do cliente"
     })
     vlrPayable: number;
+
+    @Column({ allowNull: false, comment: 'Hash gerado com o proprio objeto, Obs.: Ideia que precisa de analise'})
+    hashTransaction: string;
+
+    @ForeignKey(() => Transaction)
+    @Column({comment : 'Transação registrada.'})
+    transactionId: number
 
 }
--- src/app/service/PSPService.ts
+++ src/app/service/PSPService.ts
@@ -3,7 +3,11 @@ import {Data} from "../../util/Data";
 import {msgErro, msgPSP} from "../../util/MessageTranslate";
 import * as StringMask from 'string-mask';
 import {Transaction} from "../model/TransactionModel";
-import * as dateFns from 'date-fns'; //https://date-fns.org/
+import * as dateFns from 'date-fns';
+import PayableService from "./PayableService";
+import {Payable} from "../model/PayableModel";
+import {Column} from "sequelize-typescript"; //https://date-fns.org/
+import * as crypto from 'crypto';
 
 class PSPService {
 
@@ -38,7 +42,7 @@ class PSPService {
             let vlrApresentacaoFull =
                 (bodyRequest.vlrTransaction).toLocaleString('pt-BR',
                     { style: 'currency', currency: 'BRL' });
-
+            //Registrando a transação
             let transactionBase = await Transaction.create({
                 'vlrTransaction' : Number(bodyRequest.vlrTransaction),
                 'description' : bodyRequest.description,
@@ -46,12 +50,40 @@ class PSPService {
                 'numCard' : numCard,
                 'bearerName' : bodyRequest.bearerName,
                 'dtExpiration' : dateFns.parse(bodyRequest.dtExpiration),
-                'cvv' : bodyRequest.cvv
+                'cvv' : bodyRequest.cvv,
+
+            });
+
+            // validando status
+            let statusPayable = bodyRequest.typeTransaction == 1 ? 1 : 2;
+            let dtPayment = statusPayable == 1 ? new Date() : dateFns.addDays(new Date(), 30);
+            let percentRate = bodyRequest.typeTransaction == 1 ? 0.03 : 0.05;
+            let vlrBase = Number(bodyRequest.vlrTransaction);
+            let vlrPayablePercent = Number(bodyRequest.vlrTransaction) * Number(percentRate);
+            let vlrPayable = vlrBase - Number(vlrPayablePercent);
+            let hashTransaction = crypto.createHash('md5').update(JSON.stringify(transactionBase)).digest("hex");
+
+            // salvando os pagaveis da transação
+            let payableBase = await Payable.create({
+                'transactionId' : transactionBase.id,
+                'status': statusPayable,
+                'dtPayment': dtPayment,
+                'percentRate': percentRate,
+                'vlrPayable': vlrPayable,
+                'hashTransaction' : hashTransaction
             });
 
             result.numCard = numCard;//
             result.vlrApresentacaoFull = vlrApresentacaoFull;
+            result.statusPayable = statusPayable;
+            result.dtPayment = dtPayment;
+            result.percentRate = percentRate;
+            result.vlrBase = vlrBase;
+            result.vlrPayablePercent = vlrPayablePercent;
+            result.vlrPayable = vlrPayable;
+            result.hash =
             result.transaction = transactionBase;
+            result.payable = payableBase;
 
             data.obj = result;
             data.addMsg(tMsg.SUCCESS, "Sucesso na transação. ");
